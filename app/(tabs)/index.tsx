f3e69bcf6037a20ec959e5f3ae2f4cd4e0a455c5
ljt990218 <2511590093@qq.com>
2024-12-30 18:18:04 +0800

    chore: update file permissions and add Prettier configuration

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native'
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl">Welcome to Expo</Text>
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-2xl'>Welcome to Expo</Text>
     </View>
   )
 }
