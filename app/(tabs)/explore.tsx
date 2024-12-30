68ebff8513ca7625e6c33561939a92df775bd7b9
ljt990218 <2511590093@qq.com>
2024-12-30 17:53:52 +0800

    fix: remove unnecessary text color from welcome messages in Explore and Home screens

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 export default function TabTwoScreen() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-white text-2xl">Welcome to Explore</Text>
+      <Text className="text-2xl">Welcome to Explore</Text>
     </View>
   )
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-white text-2xl">Welcome to Expo</Text>
+      <Text className="text-2xl">Welcome to Expo</Text>
     </View>
   )
 }
