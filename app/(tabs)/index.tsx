c24ae48ebbab10c45088f1c42eb43a180639d4a5
ljt990218 <2511590093@qq.com>
2025-01-02 23:09:40 +0800

    feat(tabs): 添加待办事项列表并调整主题颜色

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,43 @@
 import { ThemedText } from '@/components/ThemedText'
 import { ThemedView } from '@/components/ThemedView'
+import { FlatList } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
 export default function HomeScreen() {
+  type TodoItem = {
+    id: number
+    title: string
+    completed: boolean
+  }
+
+  const todoList: TodoItem[] = [
+    {
+      id: 1,
+      title: 'Learn React Native',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Build an app',
+      completed: false
+    },
+    {
+      id: 3,
+      title: 'Publish the app',
+      completed: false
+    }
+  ]
 
   return (
-    <ThemedView className='flex-1 items-center justify-center'>
-      <ThemedText className='text-2xl'>Welcome to Expo</ThemedText>
-    </ThemedView>
+    <SafeAreaView className='flex-1'>
+      <ThemedView className='flex-1 items-center justify-center'>
+        <FlatList
+          className='mt-2'
+          data={todoList}
+          renderItem={({ item }) => <ThemedText className='mt-2'>{item.title}</ThemedText>}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </ThemedView>
+    </SafeAreaView>
   )
 }
