c01da53360bc9b8bde7117b8cc70dd5f67de5c5b
ljt990218 <2511590093@qq.com>
2025-06-22 17:21:20 +0800

    fix(tabs): 使用安全区域插入优化待办事项列表的布局

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { ThemedText } from '@/components/ThemedText'
 import { ThemedView } from '@/components/ThemedView'
 import { FlatList, TouchableOpacity, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useState } from 'react'
 
 export default function HomeScreen() {
+  const insets = useSafeAreaInsets()
+
   type TodoItem = {
     id: number
     title: string
@@ -41,30 +43,28 @@ export default function HomeScreen() {
   }
 
   return (
-    <SafeAreaView className='flex-1 bg-white'>
-      <ThemedView className='flex-1 items-center justify-center'>
-        <FlatList
-          className='mt-2'
-          data={todos}
-          renderItem={({ item }) => (
-            <TouchableOpacity onPress={() => toggleCompleted(item.id)}>
-              <ThemedView className='flex-row items-center p-2 gap-3'>
-                <View
-                  className={`w-6 h-6 rounded-full border-2 ${
-                    item.completed ? 'bg-green-500 border-green-500' : 'border-gray-500'
-                  }`}
-                />
-                <ThemedText
-                  className={`text-lg ${item.completed ? 'line-through text-gray-600' : ''}`}
-                >
-                  {item.title}
-                </ThemedText>
-              </ThemedView>
-            </TouchableOpacity>
-          )}
-          keyExtractor={(item) => item.id.toString()}
-        />
-      </ThemedView>
-    </SafeAreaView>
+    <ThemedView className='flex-1 items-center'>
+      <FlatList
+        style={{ paddingTop: insets.top }}
+        data={todos}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => toggleCompleted(item.id)}>
+            <ThemedView className='flex-row items-center p-2 gap-3'>
+              <View
+                className={`w-6 h-6 rounded-full border-2 ${
+                  item.completed ? 'bg-green-500 border-green-500' : 'border-gray-500'
+                }`}
+              />
+              <ThemedText
+                className={`text-lg ${item.completed ? 'line-through text-gray-600' : ''}`}
+              >
+                {item.title}
+              </ThemedText>
+            </ThemedView>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </ThemedView>
   )
 }
